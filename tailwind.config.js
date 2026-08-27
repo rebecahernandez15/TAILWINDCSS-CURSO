49bca8dd51ecb6746e568df3bc372d9729fedff5
becapeca <rebeca.delacruz@cceo.com.mx>
2026-08-27 11:39:18 -0600

    video

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,13 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: theme => ({
-        'wave-pattern': `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 23'%3E%3Cpath d='M0,0 L8,0 Q16,23 24,0 L32,0 L32,23 L0,23 Z' fill='white'/%3E%3C/svg%3E")`,
-        'wave-pattern-dots': `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 23'%3E%3Cpath d='M0,0 L8,0 Q16,23 24,0 L32,0 L32,23 L0,23 Z' fill='white'/%3E%3C/svg%3E")`
-      }),
+      'wave-pattern': `url("data:image/svg+xml,...")`,
+      'wave-pattern-dots': `url("data:image/svg+xml,...")`,
+      'pattern-characters-red': `url("../../img/pattern-characters-red.jpg")`,
+      'character-spiderman': `url("../../img/character-spiderman.png")`,
+      'wave-red': `url("../../img/wave-red.png")`,
+      'black-dots': `url("../../img/black-dots.png")`,
+    }),
       screens:{
         'xsm':'320px'
       }
