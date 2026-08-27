471245e82ec8e3dad79ffe21d92b102e2158da18
becapeca <rebeca.delacruz@cceo.com.mx>
2026-08-27 06:05:28 -0600

    fullbackground

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,10 @@ module.exports = {
       backgroundImage: theme => ({
         'wave-pattern': `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 23'%3E%3Cpath d='M0,0 L8,0 Q16,23 24,0 L32,0 L32,23 L0,23 Z' fill='white'/%3E%3C/svg%3E")`,
         'wave-pattern-dots': `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 23'%3E%3Cpath d='M0,0 L8,0 Q16,23 24,0 L32,0 L32,23 L0,23 Z' fill='white'/%3E%3C/svg%3E")`
-      })
+      }),
+      screens:{
+        'xsm':'320px'
+      }
 
     },
   },
